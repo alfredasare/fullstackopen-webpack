31ffe25ad74d482ec9a9915bbaf03183494fc9c2
alfredasare <alfredasare101@gmail.com>
2021-03-22 19:46:15 +0000

    Add global constant BACKEND_URL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
+const useNotes = url => {
+    const [notes, setNotes] = useState([]);
+    useEffect(() => {
+        axios.get(url).then(res => {
+            setNotes(res.data);
+        });
+    }, [url]);
+
+    return notes;
+};
 
 const App = () => {
     const [counter, setCounter] = useState(0);
     const [values, setValues] = useState([]);
+    const notes = useNotes(BACKEND_URL);
 
     const handleClick = () => {
         setCounter(counter + 1);
@@ -15,6 +28,9 @@ const App = () => {
             <button onClick={handleClick}>
                 press
             </button>
+            <div>
+                {notes.length} notes on the server {BACKEND_URL}
+            </div>
         </div>
     );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,45 @@
 const path = require("path");
+const webpack = require("webpack");
 
-const config = {
-    entry: ["@babel/polyfill", "./src/index.js"],
-    output: {
-        path: path.resolve(__dirname, "build"),
-        filename: "main.js"
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, "build"),
-        compress: true,
-        port: 3000,
-        open: true
-    },
-    devtool: "source-map",
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react']
+const config = (env, argv) => {
+    console.log('argv', argv.mode);
+
+    const backend_url = argv.mode === 'production'
+        ? 'https://mighty-mountain-16496.herokuapp.com/api/notes'
+        : 'http://localhost:3001/notes';
+
+    return {
+        entry: ["@babel/polyfill", "./src/index.js"],
+        output: {
+            path: path.resolve(__dirname, "build"),
+            filename: "main.js"
+        },
+        devServer: {
+            contentBase: path.resolve(__dirname, "build"),
+            compress: true,
+            port: 3000,
+            open: true
+        },
+        devtool: "source-map",
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
                 }
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
+            ]
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                BACKEND_URL: JSON.stringify(backend_url)
+            })
         ]
     }
 };
